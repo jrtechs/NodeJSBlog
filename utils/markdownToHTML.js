a35cdaadd8919e4245ab2b828b949ff1b24caf6a
jrtechs <jrtechs@hotmail.com>
2018-10-07 11:24:01 -0400

    Unified how blog posts were rendered between the preview post and full render.

--- utils/markdownToHTML.js
+++ utils/markdownToHTML.js
@@ -1,25 +1,129 @@
 const pandoc = require('node-pandoc');
-// const args = '-t html5';
 
-const args = '-S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax -t html5';
+const utils = require('../utils/utils.js');
 
-console.log("");
+const sql = require('../utils/sql');
+
+const argsFull = '-S --base-header-level=1 --toc --toc-depth=3 -N --normalize -s --mathjax -t html5';
+const argsPreview = '-S --normalize -s --mathjax -t html5';
+
+
 module.exports=
     {
-        convertToHTML: function(markdownContents)
+
+        generateBlogPost: function(post, blocks)
+        {
+            return new Promise(function(resolve, reject)
+            {
+                Promise.all([module.exports.generateBlogPostHeader(post),
+                    module.exports.generateBlogPostBody(post, blocks),
+                    module.exports.generateBlogPostFooter()]).then(function(content)
+                {
+                    resolve(content.join(''));
+                })
+            });
+        },
+
+        generateBlogPostHeader: function(post)
+        {
+
+            var htmlHead = "<div class=\"blogPost\">";
+            //image
+            if(!(post.picture_url === "n/a"))
+            {
+                htmlHead +="<img src=\"/blogContent/headerImages/" + post.picture_url +
+                    "\" alt=\"\" style=\"width:100%; height:10%\">";
+            }
+
+            htmlHead += "<div class=\"p-4\"><div class=\"\">";
+            //title
+            htmlHead += "<h3><b>" + post.name + "</b></h3>";
+            //date
+            htmlHead += "<h5><span class=\"w3-opacity\">" +
+                post.published.toDateString() + "</span></h5>";
+            htmlHead +="</div>" + "<div class=\"\">";
+
+            return htmlHead;
+        },
+
+
+        generateBlogPostBody: function(post, blocks)
+        {
+            return new Promise(function(resolve, reject)
+            {
+                sql.getCategory(post.category_id).then(function(category)
+                {
+                    const pathName =  "blogContent/posts/" + category[0].url + "/"
+                        + post.url + ".md";
+                    var markDown = utils.getFileContents(pathName).toString();
+                    markDown = markDown.split("(media/").join("(" + "../blogContent/posts/"
+                        + category[0].url + "/media/");
+
+                    module.exports.convertToHTML(markDown, 1).then(function(result)
+                    {
+
+                        result = result.split("<figcaption>").join("<figcaption style=\"visibility: hidden;\">");
+
+                        if(blocks == -1)
+                            resolve(result);
+
+                        var htmlBlocks = result.split("<p>");
+                        var html = "";
+                        for(var i = 0; i < blocks; i++)
+                        {
+                            html += "<p>" + htmlBlocks[i];
+                        }
+
+                        html += "      <div class=\"\">\n" +
+                            "          <p class='text-center'><button class=\"btn btn-secondary btn-lg "
+                            +  "onclick=\"location.href='" +
+                            "http://jrtechs.net/" + category[0].url + "/" + post.url +
+                            "'\"><b>READ MORE &raquo;</b></button></p>\n" +
+                            "      </div>\n";
+
+                        resolve(html);
+
+                    }).catch(function(error)
+                    {
+                        reject(error);
+                    })
+
+                });
+            })
+        },
+
+        generateBlogPostFooter: function()
+        {
+            return "</div></div></div><br><br>";
+        },
+
+        convertToHTML: function(markdownContents, type)
         {
             return new Promise(function(resolve, reject)
             {
                 // Set your callback function
-                callback = function (err, result)
+                callback = function (err, html)
                 {
                     if (err)
                     {
                         reject(err);
                     }
-                    resolve(result);
+
+
+                    html = html.split("<img").join("<img style=\"max-width: 100%;\" ");
+                    html = html.split("<code>").join("<code class='hljs cpp'>");
+
+
+                    resolve(html);
                 };
-                pandoc(markdownContents, args, callback);
+                if(type == -1)
+                {
+                    pandoc(markdownContents, argsFull, callback);
+                }
+                else
+                {
+                    pandoc(markdownContents, argsPreview, callback);
+                }
             });
         },
     }
