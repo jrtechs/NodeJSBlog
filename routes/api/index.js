8cdf12a4d9f9ab4d133b1d3e11fc6b33b7f02b7d
jrtechs <jeffery@jrtechs.net>
2020-01-03 21:49:45 -0500

    Updated timeline so that the blog post will render once you click on it in the time line.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,33 @@
 const routes = require('express').Router();
 const sql = require('../../utils/sql');
 
-
+const renderPost = require('../../blog/renderBlogPost');
 
 routes.get('/posts', (request, result) =>
 {
     sql.getAllPosts().then((data)=>
     {
-        result.json(data).end();result
+        result.json(data).end();
     }).catch((err)=>
     {
         result.status(500).json([]).end();
     });
+});
+
+
+routes.get('/render/:postID', (request, result) =>
+{
+    sql.getPostById(request.params.postID).then((sqlData)=>
+    {
+        renderPost.generateBlogPost(sqlData, -1).then((rendered)=>
+        { 
+            result.json(rendered).end();
+        });
+    }).catch((err)=>
+    {
+        result.status(404).json({error: 404}).end();
+    })
+
 });
 
 routes.get('*', (request, result) =>
--- utils/sql.js
+++ utils/sql.js
@@ -157,15 +157,20 @@ module.exports=
      */
     getPostById: function(id)
     {
-        console.log("select * from posts where post_id='" + id + "' limit 1");
-
-        return new Promise(function(resolve, reject)
+        return new Promise((resolve, reject)=>
         {
             fetch("select * from posts where post_id='" + id + "' limit 1")
                 .then(function(post)
             {
-                resolve(post[0]);
-            }).catch(function(error)
+                if(post.length === 1)
+                {
+                    resolve(post[0]);
+                }
+                else
+                {
+                    reject();
+                }
+            }).catch((error)=>
             {
                 reject(error);
             });
